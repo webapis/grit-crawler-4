25ab907b55197dc80e01db4274339dc74b10df98
webapis <60083568+webapis@users.noreply.github.com>
2024-01-24 12:06:25 +0500

    defacot

--- src/main.js
+++ src/main.js
@@ -2,13 +2,16 @@
 import { PuppeteerCrawler, ProxyConfiguration, RequestList } from 'crawlee';
 import { router } from './routes.js';
 import preNavigationHooks from './crawler-helper/preNavigationHooks.mjs';
-//const startUrls = ['https://www.koton.com/'];
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+require('dotenv').config()
 
 import urls from "../urls.js"
+const marka = process.env.marka
 const crawler = new PuppeteerCrawler({
     //headless: false,
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestList: await RequestList.open(null, urls.map(m => { return { url: m.url, userData: { start: true, selector: m.selector } } })),
+    requestList: await RequestList.open(null, urls.filter(f=>f.url.includes(marka)).map(m => { return { url: m.url, userData: { start: true, selector: m.selector } } })),
     requestHandler: router,
     // Comment this option to scrape the full website.
     // maxRequestsPerCrawl: 500,
