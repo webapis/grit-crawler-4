122dde31ceff759239c80cb9ad231c0a08abdbce
webapis <60083568+webapis@users.noreply.github.com>
2024-01-24 14:00:12 +0500

    algolia

---
+++ utils/importData.js
@@ -0,0 +1,56 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+require("dotenv").config();
+const algoliasearch = require("algoliasearch");
+
+const client = algoliasearch("7JF244QSZZ", process.env.ALGOLIAKEY);
+
+function setSettings(index) {
+  return new Promise((resolve, reject) => {
+    try {
+      index
+        .setSettings({
+          attributeForDistinct: "brand",
+          distinct: true,
+        })
+        .then(() => {
+          return resolve(true);
+        });
+    } catch (error) {
+      return reject(error);
+    }
+  });
+}
+
+async function importLinkData({ data,brand }) {
+  const linkIndex = client.initIndex("brand");
+  
+
+  let hits = [];
+ const result = await linkIndex.browseObjects({
+  batch: batch => {
+    hits = hits.concat(batch);
+  },
+  attributesToRetrieve: ["objectID"],
+  query: brand
+})
+debugger
+const objectsToDelete = hits.map(m=>m.objectID)
+debugger
+await linkIndex.deleteObjects(objectsToDelete)
+debugger
+//  await setSettings(linkIndex);
+  return new Promise((resolve, reject) => {
+    try {
+      linkIndex
+        .saveObjects(data, { autoGenerateObjectIDIfNotExist: true })
+        .then((d) => {
+          return resolve(d);
+        });
+    } catch (error) {
+      return reject(error);
+    }
+  });
+}
+
+export default importLinkData ;
