c7fc99abab931cb4671c8654f9ee4afee2ed7a09
webapis <60083568+webapis@users.noreply.github.com>
2024-01-24 14:00:12 +0500

    algolia

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
 // For more information, see https://crawlee.dev/
-import { PuppeteerCrawler, ProxyConfiguration, RequestList } from 'crawlee';
+import { PuppeteerCrawler, ProxyConfiguration, RequestList,Dataset } from 'crawlee';
 import { router } from './routes.js';
 import preNavigationHooks from './crawler-helper/preNavigationHooks.mjs';
 import { createRequire } from 'module';
+import importLinkData from '../utils/importData.js'
 const require = createRequire(import.meta.url);
+
 require('dotenv').config()
 
 import urls from "../urls.js"
+
 const marka = process.env.marka
 const crawler = new PuppeteerCrawler({
     //headless: false,
@@ -14,9 +17,13 @@ const crawler = new PuppeteerCrawler({
     requestList: await RequestList.open(null, urls.filter(f=>f.url.includes(marka)).map(m => { return { url: m.url, userData: { start: true, selector: m.selector } } })),
     requestHandler: router,
     // Comment this option to scrape the full website.
-    // maxRequestsPerCrawl: 500,
+// maxRequestsPerCrawl: 5,
     maxConcurrency: 2,
     preNavigationHooks,
 });
 
 await crawler.run();
+const dataDataset = await Dataset.open();
+const { items} = await dataDataset.getData();
+await importLinkData({data:items.map(m=>{return {...m,brand:marka}}),brand:marka })
+console.log('items---',items)
