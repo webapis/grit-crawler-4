c46465f94fae0e8778a336004bd7a66400215ec4
webapis <60083568+webapis@users.noreply.github.com>
2024-01-29 12:42:40 +0500

    beyyoglu

--- urls.js
+++ urls.js
@@ -18,8 +18,8 @@ export default [
     { url: 'https://www.beymen.com/home/sitemap', selector:'.m-siteMap a.m-siteMap__link.navLink', pageSelector:'#productList' },
    { url: 'https://www.beymenclub.com/kadin-30058',selector:'a.m-subNav__link', pageSelector:'#productList' },
    { url: 'https://www.beymenclub.com/erkek-30060',selector:'a.m-subNav__link', pageSelector:'#productList' },
-   { url: 'https://www.beyyoglu.com/kadin/', pageSelector:'.js-product-wrapper.product-item' },
-   { url: 'https://www.beyyoglu.com/erkek/', pageSelector:'.js-product-wrapper.product-item' },
+   { url: 'https://www.beyyoglu.com/kadin/',selector:'.list__header-categories a', pageSelector:'.js-product-wrapper.product-item' },
+   { url: 'https://www.beyyoglu.com/erkek/',selector:'.list__header-categories a', pageSelector:'.js-product-wrapper.product-item' },
    { url: 'https://www.blacknoble.com/erkek?currency=try', pageSelector:'.js-product-wrapper.product-item' },
 ]
 
