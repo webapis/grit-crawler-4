5e24fdf8b79e596f712443a4a7443ac0b0a74edd
webapis <60083568+webapis@users.noreply.github.com>
2024-01-29 14:35:28 +0500

    Update bsl.cjs

--- src/handlers/bsl.cjs
+++ src/handlers/bsl.cjs
@@ -8,14 +8,14 @@ async function handler(page) {
     const data = await page.$$eval('.Prd', (productCards) => {
         return productCards.map(document => {
           //  const priceNew =document.querySelectorAll('.PriceArea span')? Array.from(document.querySelectorAll('.PriceArea span')).reverse()[0].innerText.replace('TL', '').trim():null//.replace(',','.')
-            const link =document.querySelector('button.AddToCart')? document.querySelector('button.AddToCart').getAttribute('data-producturl'):null
+         //   const link =document.querySelector('button.AddToCart')? document.querySelector('button.AddToCart').getAttribute('data-producturl'):null
             const imageUrl =document.querySelector('[data-src]')? document.querySelector('[data-src]').getAttribute('data-src'):null
             const title =document.querySelector('button.AddToCart')? document.querySelector('button.AddToCart').getAttribute('data-productname') + ' ' + document.querySelector('button.AddToCart').getAttribute('data-colorname'):null
             return {
-                title: 'bsl ' + title.replace(/İ/g, 'i').toLowerCase(),
+               // title: 'bsl ' + title.replace(/İ/g, 'i').toLowerCase(),
               //  priceNew,
                 imageUrl,
-                link,
+             //   link,
                 timestamp: Date.now(),
                 marka: 'bsl',
             }
